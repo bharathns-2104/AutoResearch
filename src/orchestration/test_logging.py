bfaf149924f9d4c99211af67d4866d4877bd11dc
Bharath <nsbharath04@gmail.com>
2026-02-20 14:28:28 +0530

    Report Generation

--- src/orchestration/test_logging.py
+++
@@ -1,7 +0,0 @@
-from .logger import setup_logger
-
-logger = setup_logger()
-
-logger.info("AutoResearch system initialized")
-logger.debug("Debug mode active")
-logger.error("Sample error Message")
